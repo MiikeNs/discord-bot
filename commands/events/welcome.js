fe9098e0116f6a375dde65eb01a24bb1c56ba72a
Miguel Nunes <nunes13.gofy@gmail.com>
2025-12-07 05:56:01 +0000

    Fixed applicant role assign, wrong syntax

--- commands/events/welcome.js
+++ commands/events/welcome.js
@@ -3,6 +3,8 @@ const { guest_role_id, applicantRoleId } = require('../../config/roles.js');
 
 const welcome_channel_id = '1441610605344719000';
 
+let welcomeMessageId = null;
+
 module.exports = (client) => {
   client.on('guildMemberAdd', async (member) => {
     const role = member.guild.roles.cache.get(guest_role_id);
@@ -10,13 +12,15 @@ module.exports = (client) => {
 
     const channel = member.guild.channels.cache.get(welcome_channel_id);
     const sent_message = await channel.send(`Welcome ${member.user.username}!`);
+    welcomeMessageId = sent_message.id;
     const welcome_reaction = await sent_message.react(welcomeMessageEmoji);
   });
 };
 
 module.exports = (client) => {
-  client.on('messageReactionAdd', async (reaction, message) => {
-    if (message === sent_message && reaction.emoji.name === welcomeMessageEmoji) {
+  client.on('messageReactionAdd', async (reaction, user) => {
+    if (reaction.message.id === welcomeMessageId && reaction.emoji.name === welcomeMessageEmoji) {
+      const member = reaction.message.guild.members.cache.get(user.id);
       await member.roles.add(applicantRoleId);
     };
   });
